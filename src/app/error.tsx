f65835e14c20915b6ee242bb5e1af6b2b5e2a0b9
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-08-14 00:53:56 +0300

    edit loading in the app

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,55 +1,57 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { MoveRight, RefreshCcw } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { Button } from "@/components/ui/button";
+import { MoveRight, RefreshCcw } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Error({
-	error,
-	reset,
+  error,
+  reset,
 }: {
-	error: Error & { digest?: string };
-	reset: () => void;
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
-	const router = useRouter();
+  const router = useRouter();
 
-	useEffect(() => {
-		// Log the error to an error reporting service
-		console.error(error.message);
-	}, [error]);
-	return (
-		<main
-			className='flex flex-col justify-center items-center h-screen text-4xl sm:text-6xl text-rayanSecondary-dark'
-			dir='rtl'
-		>
-			<div className='py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16'>
-				<h1 className='mb-4 text-destructive text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl'>
-					حدثت مشكلة ما
-				</h1>
-				<div className='mb-8 text-destructive'>
-					يرجى المحاولة لاحقا
-				</div>
-				<div className='flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0'>
-					<Button
-						className='px-8 bg-rayanSecondary-dark dark:bg-rayanSecondary-light'
-						size='lg'
-					>
-						<RefreshCcw className='ml-4' />
-						أعد المحاولة
-					</Button>
-				</div>
-				<div className='flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 mt-4'>
-					<Button
-						className='px-8 bg-rayanSecondary-dark dark:bg-rayanSecondary-light'
-						size='lg'
-						onClick={() => router.replace('/')}
-					>
-						<MoveRight className='ml-4' />
-						الرئيسية{' '}
-					</Button>
-				</div>
-			</div>
-		</main>
-	);
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "production") console.error(error.message);
+  }, [error]);
+
+  if (process.env.NODE_ENV === "production") router.push("/notFound");
+  return (
+    <main
+      className="flex h-screen flex-col items-center justify-center text-4xl text-rayanSecondary-dark sm:text-6xl"
+      dir="rtl"
+    >
+      <div className="mx-auto max-w-screen-xl px-4 py-8 text-center text-2xl lg:py-16">
+        <div className="mb-8 text-destructive">
+          <h6 className="">name: {error.name}</h6>
+        </div>
+        <h1 className="mb-4 leading-none tracking-tight text-destructive">
+          {error.message}
+        </h1>
+
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+          <Button
+            className="bg-rayanSecondary-dark px-8 dark:bg-rayanSecondary-light"
+            size="lg"
+          >
+            <RefreshCcw className="ml-4" />
+            أعد المحاولة
+          </Button>
+        </div>
+        <div className="mt-4 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+          <Button
+            className="bg-rayanSecondary-dark px-8 dark:bg-rayanSecondary-light"
+            size="lg"
+            onClick={() => router.replace("/")}
+          >
+            <MoveRight className="ml-4" />
+            الرئيسية{" "}
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
