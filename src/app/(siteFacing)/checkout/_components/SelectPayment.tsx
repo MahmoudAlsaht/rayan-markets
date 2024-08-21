66d8f2eb0f16ea260a5db790efb2868867d9f9b8
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-08-21 19:56:35 +0300

    select contact address

---
+++ src/app/(siteFacing)/checkout/_components/SelectPayment.tsx
@@ -0,0 +1,18 @@
+import { GrAtm } from "react-icons/gr";
+
+export default function SelectPayment() {
+  return (
+    <section className="flex flex-col gap-3">
+      {/* <div>
+        <div className="w-full bg-slate-200" />
+        <GrAtm />
+      </div>
+      <div>
+        <GrAtm />
+      </div>
+      <div>
+        <GrAtm />
+      </div> */}
+    </section>
+  );
+}
