f7172848f804afa28f296c80efee8cd368297f8a
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-08-28 19:33:28 +0300

    edit delete district

--- src/app/admin/settings/districts/_actions/deleteDistrict.ts
+++ src/app/admin/settings/districts/_actions/deleteDistrict.ts
@@ -2,10 +2,31 @@
 import db from "@/db/db";
 
 export const deleteDistrict = async (id: string) => {
-  await db.district.delete({
+  const district = await db.district.findUnique({
     where: { id },
+    select: { id: true, contacts: { select: { profileId: true, id: true } } },
   });
+
+  if (!district) return;
+
+  if (district?.contacts)
+    for (const contact of district.contacts) {
+      if (contact.profileId)
+        await db.profile.update({
+          where: { id: contact.profileId },
+          data: {
+            contacts: {
+              disconnect: { id: contact.id },
+            },
+          },
+        });
+    }
+
   await db.contact.deleteMany({
     where: { districtId: id },
+  });
+
+  await db.district.delete({
+    where: { id: district.id as string },
   });
 };
