e01edd88e820f86c6f99afb87442a8b3c383e965
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-09-08 15:51:32 +0300

    ignore upload-data-via-sheets

--- src/app/(siteFacing)/upload-data-via-sheets/_actions/uploadData.ts
+++ src/app/(siteFacing)/upload-data-via-sheets/_actions/uploadData.ts
@@ -21,66 +21,11 @@ type Data = {
   description?: string;
 };
 
-const cloudImages = [
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201456/Test%20images/xlcbixz3yf52y6u9d8nb.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201455/Test%20images/hhnnsuzq6hwlt2ue7krb.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201455/Test%20images/tofunqoc8mnv9bkwx4jk.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201454/Test%20images/yowctmsr0rb4uatoylki.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201454/Test%20images/guobcritzvvclpbcbbin.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201454/Test%20images/ihsyqvwbkqv9kgmu5glv.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201453/Test%20images/epdfle4zgewrt0eed6bm.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201453/Test%20images/m5o3fqdo053tttnd1azl.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201453/Test%20images/ny2ocpwceyei6r9mobmy.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201454/Test%20images/ihsyqvwbkqv9kgmu5glv.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201453/Test%20images/epdfle4zgewrt0eed6bm.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201453/Test%20images/m5o3fqdo053tttnd1azl.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201451/Test%20images/m4nlizipjuhdpxqtxaow.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201451/Test%20images/y1hax9eory7erjmjuxks.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201453/Test%20images/pjlqjhkm4bkjoednsqko.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201453/Test%20images/ny2ocpwceyei6r9mobmy.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201449/Test%20images/csymnsufnfjstik0bk9z.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201450/Test%20images/nri5umywcabflq9cshes.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201451/Test%20images/wjy8m5zj9xwe38izo6an.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201451/Test%20images/wjy8m5zj9xwe38izo6an.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201451/Test%20images/ubw1m5hvodphzescfmbi.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201449/Test%20images/csymnsufnfjstik0bk9z.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201450/Test%20images/nri5umywcabflq9cshes.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201451/Test%20images/wjy8m5zj9xwe38izo6an.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201451/Test%20images/ubw1m5hvodphzescfmbi.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201448/Test%20images/dejwefl2nihi5q4ye4nm.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201448/Test%20images/eslwduwxv7afnyu6kk9o.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201448/Test%20images/o7ydvclhk4hwyonqq6yf.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201449/Test%20images/du2g00x8ycmi5l0exqwq.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201447/Test%20images/toludcmvomxdd6kzpfbl.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201447/Test%20images/qho5ysts8ku4lowupehs.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201446/Test%20images/zheh4pgqrqbbj3jnckvi.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201446/Test%20images/idib1wrrvpnnpchmy82g.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201444/Test%20images/okzucdeyi6stzpkhpjoq.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201445/Test%20images/isn8u8be4fysjvixx6oe.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201445/Test%20images/mdocea8fwlbqvowjbbrk.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201446/Test%20images/u413z3ascsvs28lr6edf.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201444/Test%20images/xk7jmeu5ezbphjaet2qn.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201444/Test%20images/jv8yigzwxmtrjb4wemt5.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201444/Test%20images/lsr6pnjavfsoz1c3ftrc.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201443/Test%20images/xu7z1pqadnwrctvuqyog.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201441/Test%20images/i8buqhgca2b4utjnq5kd.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201441/Test%20images/xaxe82melinjlgpkbujv.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201442/Test%20images/yjxbbuzjbgvvcbjromt3.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201443/Test%20images/ijocrr5kewd11slm5kwd.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201441/Test%20images/pvdfg0clbtm5mj8gu9mb.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201441/Test%20images/jmjc6vrylpekwcsfy8vp.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201441/Test%20images/haqfja5iix71llcavqhw.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201441/Test%20images/rim89c6idkymjalapjoe.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201438/Test%20images/gkci2rdui6pb9ikpjoaj.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201438/Test%20images/yptj2eaakjzywg8v1kia.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201438/Test%20images/gkci2rdui6pb9ikpjoaj.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201438/Test%20images/ok1cg45yq5yovljssbht.png",
-  "https://res.cloudinary.com/itsformemahmoud2097/image/upload/v1725201438/Test%20images/ok1cg45yq5yovljssbht.png",
-];
-
 function randomize(arr) {
   return Math.floor(Math.random() * (arr.length - 1));
 }
+
+const prices = [0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75];
 
 export async function generateData(fileData: any[]) {
   const data: Data[] = fileData;
@@ -206,7 +151,11 @@ async function generateProduct(product: Data) {
             : product.productType,
         price:
           !product.price || product.price === ""
-            ? 1.0
+            ? product.newPrice && product.newPrice !== ""
+              ? formatPrice(
+                  parseFloat(product.newPrice) + randomize(prices).toString(),
+                )
+              : formatPrice("1.0")
             : formatPrice(product.price),
         isOffer: checkIsOffer(product.offerStartsAt, product.offerEndsAt),
         newPrice: checkIsOffer(product.offerStartsAt, product.offerEndsAt)
