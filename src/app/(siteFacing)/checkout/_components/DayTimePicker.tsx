62c623c23cc7a5f170ba33e50269825bdffadd61
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-09-08 15:07:12 +0300

    hide calender navigation buttons

--- src/app/(siteFacing)/checkout/_components/DayTimePicker.tsx
+++ src/app/(siteFacing)/checkout/_components/DayTimePicker.tsx
@@ -77,6 +77,7 @@ export default function DayTimePicker({
         </label>
       </div>
       <Calendar
+        hideNavigation
         mode="single"
         required
         selected={selected}
