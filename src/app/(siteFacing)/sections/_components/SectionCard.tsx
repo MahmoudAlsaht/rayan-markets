e4c783fbc7765ff99021581102556f6b2d65c505
Mahmoud <60844546+MahmoudAlsaht@users.noreply.github.com>
2024-09-17 11:36:37 +0300

    Update SectionCard.tsx

--- src/app/(siteFacing)/sections/_components/SectionCard.tsx
+++ src/app/(siteFacing)/sections/_components/SectionCard.tsx
@@ -1,8 +1,5 @@
-'use client'
 import Image from "next/image";
 import { LoadingLink } from "@/app/(siteFacing)/_context/LoadingContext";
-import { Card, CardContent } from "@/components/ui/card";
-import {useState} from 'react';
 
 export type SectionCardProps = {
   id: string;
@@ -18,39 +15,25 @@ export default function SectionCard({
 }: {
   section: SectionCardProps;
 }) {
-    const [showName, setShowName] = useState(false); 
-  
-   const handleShowName = () => setShowName(true); 
-   const handleHideName = () => setShowName(false); 
-  
-   return ( 
-     <Card 
-       className="rounded-3xl bg-inherit" 
-       onMouseOver={handleShowName} 
-       onMouseLeave={handleHideName} 
-     > 
-       <LoadingLink href={`/sections/${section.type}/${section.id}`}> 
-         <CardContent className="relative flex aspect-square w-full items-center justify-center p-6"> 
-           <Image 
-             fill 
-             className="rounded-3xl" 
-             src={ 
-               section.cover?.path.replace("/upload", "/upload/w_200") as string 
-             } 
-             alt={`${section.name}'s image`} 
-           /> 
-           {showName && ( 
-             <span className="absolute bottom-0 hidden h-full w-full flex-col justify-center rounded-3xl bg-slate-50/0 p-1 py-0 text-center text-sm font-semibold text-rayanPrimary-dark duration-500 hover:bg-slate-50/85 sm:flex sm:text-lg"> 
-               {section.name} 
-             </span> 
-           )} 
-           <span className="absolute bottom-0 flex h-1/3 w-full flex-col justify-center rounded-md rounded-bl-3xl rounded-br-3xl bg-slate-200/85 p-1 py-0 text-center text-xs font-semibold text-rayanPrimary-dark sm:hidden"> 
-             {section.name} 
-           </span> 
-         </CardContent> 
-       </LoadingLink> 
-     </Card> 
-   );
+  return (
+    <div className="cursor-pointer rounded-3xl duration-500 sm:w-full sm:hover:scale-105 sm:hover:shadow-xl">
+      <LoadingLink href={`/sections/${section.type}/${section.id}`}>
+        <div className="relative h-24 sm:h-44">
+          <Image
+            fill
+            priority
+            className="rounded-3xl object-cover"
+            src={
+              section?.cover?.path.replace("/upload", "/upload/w_200") as string
+            }
+            alt={`${section.name}'s image`}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+        <h3 className="text-center text-lg sm:text-2xl">{section.name}</h3>
+      </LoadingLink>
+    </div>
+  );
 }
 
 export function SectionCardSkeleton() {
